9e2922b00662a51eae5ecd2414044ecf4141abb2
Javier Domi­nguez <javier.dominguez.1798@gmail.com>
2020-05-10 22:30:15 +0200

    Añadida Clase Cuadrado

---
+++ EjFiguras/src/ejfiguras/Cuadrado.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejfiguras;
+
+/**
+ *
+ * @author il_ja
+ */
+public class Cuadrado implements Figura {
+    
+}
